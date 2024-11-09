a2e2b20002d0270c83155f8225154bc48d14d579
MarcoEstSerrano <marcoesteban116@hotmail.com>
2024-11-09 11:05:46 -0600

    123

--- Proyecto Java/src/proyecto/java/Main.java
+++ Proyecto Java/src/proyecto/java/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) {
         mostrarBannerMBT();
 
-        
         int opcionMenuPrincipal;
         do {
             // Menú principal con opciones
